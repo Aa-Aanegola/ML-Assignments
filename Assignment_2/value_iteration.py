78789ea3fb90308b924ccbf1145d644d3ad48420
Aakash-AA <aakash.aanegola@gmail.com>
2021-03-27 17:44:32 +0530

    Added action check

--- Assignment_2/value_iteration.py
+++ Assignment_2/value_iteration.py
@@ -50,5 +50,44 @@ for i in range(len(pos)):
             dim1.append(dim2)
         dim0.append(dim1)
     value.append(dim0)
-    
-print(value[pos['N']][mat['1']][arrows['1']][MM['R']][health['25']][action['GATHER']])
+
+def actionPossible(pos, mat, arw, MM, hel, act):
+    if health == 0 and act != "NONE":
+        return False
+    if act == "UP":
+        if pos == "C" or pos == "S":
+            return True
+        return False
+    if act == "DOWN":
+        if pos == "C" or pos == "N":
+            return True
+        return False
+    if act == "LEFT":
+        if pos == "C" or pos == "E":
+            return True 
+        return False
+    if act == "RIGHT":
+        if pos == "C" or pos == "W":
+            return True
+        return False
+    if act == "STAY":
+        return True
+    if act == "SHOOT":
+        if arw != "0" and pos != "N" and pos != "S":
+            return True
+        return False
+    if act == "HIT":
+        if pos == "C" or pos == "E":
+            return True
+        return False
+    if act == "CRAFT":
+        if pos == "N" and mat != "0" and arw != "3":
+            return True 
+        return False
+    if act == "GATHER":
+        if pos == "S" and mat != "2":
+            return True
+    if act == "NONE":
+        if health == "0":
+            return True
+        return False 
